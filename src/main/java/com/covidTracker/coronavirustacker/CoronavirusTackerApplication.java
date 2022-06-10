f1032987dbc90df9098bd8ac5d334b5c7a339a9e
prateek <aprateek006@gmail.com>
2022-06-10 11:45:09 -0700

    First Commit adding project

---
+++ src/main/java/com/covidTracker/coronavirustacker/CoronavirusTackerApplication.java
@@ -0,0 +1,13 @@
+package com.covidTracker.coronavirustacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covidTracker/coronavirustacker/CoronavirusTackerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.covidTracker.coronavirustacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTackerApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
